17a4c5a4c42ed75a526014b01e08e0676700491d
kurtcunningham <cunningham.kurt@gmail.com>
2014-05-01 13:33:04 -0500

    updated responsive styles

--- js/browserResize.js
+++ js/browserResize.js
@@ -1,13 +1,13 @@
-$(function(){
-	var resizePage = function(){
-		$('.page-position-center').css({
-        position:'absolute',
-        left: ($(window).width() - $('.page-position-center').outerWidth())/2,
-        top: ($(window).height() - $('.page-position-center').outerHeight())/2
-    });
-	};
+// $(function(){
+// 	var resizePage = function(){
+// 		$('.page-position-center').css({
+//         position:'absolute',
+//         left: ($(window).width() - $('.page-position-center').outerWidth())/2,
+//         top: ($(window).height() - $('.page-position-center').outerHeight())/2
+//     });
+// 	};
 	
-	resizePage();
+// 	resizePage();
 
-	$(window).resize(resizePage);
-});
+// 	$(window).resize(resizePage);
+// });
